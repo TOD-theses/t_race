5881d262bbd799325ed21da88a05e6a16a2dbd89
A_A <21040751+Otto-AA@users.noreply.github.com>
2024-08-04 12:58:15 +0200

    chore: remove outdated scripts

--- scripts/exceptions_trace.sh
+++
@@ -1,2 +0,0 @@
-# find results with exceptions and create traces for these tx pairs
-grep -lR 'exception' out/results/ | sed -E 's/^.*(0x\w+)_(0x\w+).*$/\1,\2/' > /tmp/hashes.csv; t_race trace --transactions-csv /tmp/hashes.csv
--- scripts/tod_opcodes.sh
+++
@@ -1,2 +0,0 @@
-# count the instructions that cause TOD
-grep -zRoP '"found": true(.*\n){8}' out/results | grep --text opcode | grep --text -oP '\d+$' | sort | uniq -c
